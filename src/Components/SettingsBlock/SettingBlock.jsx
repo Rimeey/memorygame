8421de85f5298341d6e92f89e1236cfa450a5c17
Rimey <madmanspoiler277@gmail.com>
2024-04-14 00:27:22 +0300

    adaptive

--- src/Components/SettingsBlock/SettingBlock.jsx
+++ src/Components/SettingsBlock/SettingBlock.jsx
@@ -4,33 +4,41 @@ import { Memory } from '../../Context';
 
 export default function SettingsBlock() {
 
-    const { handle_click_size, handle_click_sound, handle_click_speed} = useContext(Memory);
+    const { handle_click_size, handle_click_sound, handle_click_speed } = useContext(Memory);
 
     return (
         <div className="settings">
             <div className="settings_element">
-                <h2>Size: </h2>
-                <div className="setting button dark" onClick={(e) => handle_click_size(e)}>4x3</div>
-                <div className="setting button" onClick={(e) => handle_click_size(e)}>4x4</div>
-                <div className="setting button" onClick={(e) => handle_click_size(e)}>5x4</div>
-                <div className="setting button" onClick={(e) => handle_click_size(e)}>6x5</div>
-                <div className="setting button" onClick={(e) => handle_click_size(e)}>6x6</div>
+                <h2>Size:</h2>
+                <div className="setting">
+                    <div className="button dark" onClick={(e) => handle_click_size(e)}>4x3</div>
+                    <div className="button" onClick={(e) => handle_click_size(e)}>4x4</div>
+                    <div className="button" onClick={(e) => handle_click_size(e)}>5x4</div>
+                    <div className="button" onClick={(e) => handle_click_size(e)}>6x5</div>
+                    <div className="button" onClick={(e) => handle_click_size(e)}>6x6</div>
+                </div>
             </div>
             <div className="settings_element">
-                <h2>Sound: </h2>
-                <div className="setting button dark" onClick={(e) => handle_click_sound(e)}>On</div>
-                <div className="setting button" onClick={(e) => handle_click_sound(e)}>Off</div>
+                <h2>Sound:</h2>
+                <div className="setting">
+                    <div className="button dark" onClick={(e) => handle_click_sound(e)}>On</div>
+                    <div className="button" onClick={(e) => handle_click_sound(e)}>Off</div>
+                </div>
             </div>
             <div className="settings_element">
-                <h2>Speed: </h2>
-                <div className="setting button dark" onClick={(e) => handle_click_speed(e)}>Fast</div>
-                <div className="setting button" onClick={(e) => handle_click_speed(e)}>Standart</div>
-                <div className="setting button" onClick={(e) => handle_click_speed(e)}>Slow</div>
+                <h2>Speed:</h2>
+                <div className="setting">
+                    <div className="button dark" onClick={(e) => handle_click_speed(e)}>Fast</div>
+                    <div className="button" onClick={(e) => handle_click_speed(e)}>Standart</div>
+                    <div className="button" onClick={(e) => handle_click_speed(e)}>Slow</div>
+                </div>
             </div>
             <div className="settings_element">
-                <h2>Design: </h2>
-                <div className="setting button dark">Dark</div>
-                <div className="setting button">Light</div>
+                <h2>Design:</h2>
+                <div className="setting">
+                    <div className="button dark">Dark</div>
+                    <div className="button">Light</div>
+                </div>
             </div>
         </div>
     );
